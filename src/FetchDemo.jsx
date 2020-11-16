a67e117fed817b369c2416465dc2aff7f1554776
yq0216 <1294642921@qq.com>
2020-11-16 22:08:37 +0800

    fecth post请求

--- src/FetchDemo.jsx
+++ src/FetchDemo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import qs from "qs";
 
 export default class FetchDemo extends React.Component {
+  // http://iwenwiki.com/api/
   constructor(props) {
     super(props);
     this.state = {
@@ -9,12 +11,41 @@ export default class FetchDemo extends React.Component {
   }
 
   componentDidMount() {
+    // get请求
     fetch("http://iwenwiki.com/api/blueberrypai/getIndexBanner.php")
       .then((res) => res.json())
       .then((data) => {
         this.setState({
           banners: data.banner,
         });
+      });
+
+    // post请求
+    fetch("http://iwenwiki.com/api/blueberrypai/login.php", {
+      // body: {
+      //   user_id: "iwen@qq.com",
+      //   password: "iwen123",
+      //   verification_code: "crfvw",
+      // },
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Accept: "application/json,text/plain,*/*",
+      },
+
+      // 第一种写法
+      // body: "user_id=iwen@qq.com&password=iwen123&verification_code=crfvw",
+
+      // 第二种写法
+      body: qs.stringify({
+        user_id: "iwen@qq.com",
+        password: "iwen123",
+        verification_code: "crfvw",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
       });
   }
 
